3ce5852ee4cb45ca7061c01bdfb452da40a4f7b5
jiajia0827 <2268977136@qq.com>
2026-01-26 14:28:51 +0800

    第一次提交

---
+++ src/main/java/org/example/ai_driven_project_management_intelligence_platform/AiDrivenProjectManagementIntelligencePlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.ai_driven_project_management_intelligence_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDrivenProjectManagementIntelligencePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDrivenProjectManagementIntelligencePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ai_driven_project_management_intelligence_platform/AiDrivenProjectManagementIntelligencePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ai_driven_project_management_intelligence_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDrivenProjectManagementIntelligencePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
